c1f9ab7a8a425dba2ff41a744be8f33fa7afc343
María Gómez <mgomgar132@g.educaand.es>
2026-05-17 19:47:56 +0200

    [add] añade estructura base del proyecto y prueba de conectividad HolaMundoTest

---
+++ src/test/java/HolaMundoTest.java
@@ -0,0 +1,21 @@
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class HolaMundoTest {
+    @Test
+    public void holaMundo() {
+        String var = "Hola mundo";
+        
+        assertEquals(var, "Hola mundo");
+    }
+}
